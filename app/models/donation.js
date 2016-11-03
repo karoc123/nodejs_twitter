12e7cf5e4da47bd3f695f3d4ad46d5497d828265
Tobias Zink <zinktobias@googlemail.com>
2016-11-03 08:20:18 +0100

    first commit

---
+++ app/models/donation.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const donationSchema = mongoose.Schema({
+  amount: Number,
+  method: String,
+  donor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+const Donation = mongoose.model('Donation', donationSchema);
+module.exports = Donation;
